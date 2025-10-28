60ba4a5382de7861d54acab9b0550de036c480cf
razoring <blotzytgaming@gmail.com>
2025-10-28 03:09:49 -0400

    readme

--- src/nodenet/node.py
+++ src/nodenet/node.py
@@ -9,7 +9,7 @@ import random
 import math
 
 class encryption():
-    def __init__(self, min=1000, max=9999):
+    def __init__(self, min=1000, max=16921):
         self.MIN = min
         self.MAX = max
         self.privateKey = 0
