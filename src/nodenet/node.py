5a2354d1ab815c9193b7fa7951767aa77bd4a394
razoring <blotzytgaming@gmail.com>
2025-10-27 23:21:56 -0400

    Update node.py

--- src/nodenet/node.py
+++ src/nodenet/node.py
@@ -8,6 +8,53 @@ import os
 import random
 import math
 
+class RSA:
+    def __init__(self, min_prime_val=1000, max_prime_val=9999):
+        p = self._prime(min_prime_val, max_prime_val)
+        q = self._prime(min_prime_val, max_prime_val)
+
+        while p == q:
+            q = self._prime(min_prime_val, max_prime_val)
+
+        self.modulus = p * q
+        phi = (p - 1) * (q - 1)
+
+        self.public_exponent = random.randint(3, phi - 1)
+        while math.gcd(self.public_exponent, phi) != 1:
+            self.public_exponent = random.randint(3, phi - 1)
+
+        self.private_exponent = self._modInverse(self.public_exponent, phi)
+
+    def _isPrime(self, n):
+        if n < 2:
+            return False
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    def _prime(self, min_val, max_val):
+        prime_candidate = random.randint(min_val, max_val)
+        while not self._isPrime(prime_candidate):
+            prime_candidate = random.randint(min_val, max_val)
+        return prime_candidate
+
+    def _modInverse(self, e, phi):
+        for d in range(3, phi):
+            if (d * e) % phi == 1:
+                return d
+        raise ValueError("Modular inverse does not exist")
+
+    def encrypt(self, plaintext):
+        encoded_chars = [ord(char) for char in plaintext]
+        ciphertext = [pow(char, self.public_exponent, self.modulus) for char in encoded_chars]
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        decoded_chars = [pow(char, self.private_exponent, self.modulus) for char in ciphertext]
+        plaintext = "".join(chr(char) for char in decoded_chars)
+        return plaintext
+
 class Nodenet():
     def __init__(self, host, port, nickname):
         self.HEADER_LEN = 512
